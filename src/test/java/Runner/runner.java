287f814722b9dbf2054621c7849ef9122dcb364c
nkosi <nkosi.cele@simplify.africa>
2024-10-01 19:22:52 +0200

    updated the runner with the publick section

--- src/test/java/Runner/runner.java
+++ src/test/java/Runner/runner.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
         features = {"src/test/resources/Features"},
         glue = {"Steps"},
         plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
-        tags =  "@EndToEnd"
+        tags =  "@EndToEnd",publish = true
 )
 public class runner extends AbstractTestNGCucumberTests {
 }
